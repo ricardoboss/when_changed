a617e4148e098b2a707cd023882b3008a26f004f
Ricardo Boss <contact@ricardoboss.de>
2022-06-06 14:52:32 +0200

    Updated project configurations

--- when_changed/State.cs
+++ when_changed/State.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace when_changed
+﻿namespace when_changed
 {
-    enum State
+    internal enum State
     {
         Watching,
         WaitingToExecute,
